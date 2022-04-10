1867268abe8a8bbed73f173d7e54b157c35258df
Rosivan Batista <jrosivan@gmail.com>
2022-04-10 20:39:35 -0300

    Criação da frota, ordenação e listagem

--- src/LocadoraApp.java
+++ src/LocadoraApp.java
@@ -6,10 +6,30 @@ public class LocadoraApp  {
 
     public static void main(String[] args) {
 
-        System.out.println("--------------------------------");
-        System.out.println("PANGEIA RENT A CAR");
-        System.out.println("--------------------------------");
+        System.out.println("┌".concat("─".repeat(50)).concat("┐"));
+        System.out.println("│                PANGEIA RENT A CAR                │");
+        System.out.println("└".concat("─".repeat(50)).concat("┘"));
 
+
+        System.out.println("1) Comprando meus carros...");
+        frota[0] = new Carro("JRB-6591","FIAT", "CROSS", 35.00, true);
+        frota[1] = new Carro("BOB-3333","WOLKS", "HB20", 120.00, true);
+        frota[2] = new Carro("DIK-1234","ALFA", "SUV", 350.00, true);
+        frota[3] = new Carro("PAT-8970","CHERRY", "HATCH", 435.00, false);
+        frota[4] = new Carro("MIK-1358","TOYOTA", "SW4", 275.00, true);
+        frota[5] = new Carro("AAA-4798","FIAT", "SEDAN", 217.00, true);
+        frota[6] = new Carro("BBB-1358","WOLKS", "SUV", 312.00, false);
+        frota[7] = new Carro("CCC-6328","TOYOTA", "AX30", 123.00, true);
+        frota[8] = new Carro("DDD-5638","TESLA", "SW4", 271.00, false);
+        frota[9] = new Carro("EEE-2318","TIGUAN", "HATCH", 83.00, true);
+        System.out.println("   ......feito! (comprei 10).....");
+
+        System.out.println("2) Preciso organizar eles, por valor de aluguel...");
+        frota = ordenarVeiculos(frota);
+        System.out.println("   ......feito!.....");
+
+        System.out.println("3) Listando minha frota:");
+        System.out.println(Arrays.toString(frota));
 
     }
 
@@ -17,7 +37,5 @@ public class LocadoraApp  {
         Arrays.sort(veiculos);
         return veiculos;
     }
-
-
 
 }
